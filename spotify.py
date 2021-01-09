9275451ebe2c3cf2c824d6da3657257ea5e8c970
Juan Pablo Rodriguez Morales <uncorreodeinternet@gmail.com>
2021-01-08 19:52:26 -0500

    Refactored spotify file to be a class

--- spotify.py
+++ spotify.py
@@ -4,25 +4,55 @@ import os
 from dotenv import load_dotenv
 import base64
 
-load_dotenv()
-
 API_TOKEN_URL = "https://accounts.spotify.com/api/token"
 
-async def get_new_token():
-    requestor = Requestor()
+class Spotify():
 
-    message = os.environ['SPOTIFY_CLIENT_ID']+':'+os.environ['SPOTIFY_CLIENT_SECRET']
-    message_bytes = message.encode('ascii')
-    base64_bytes = base64.b64encode(message_bytes)
-    base64_message = 'Basic '+ base64_bytes.decode('ascii')
+    def __init__(self):
+        load_dotenv()
+        self._refresh_token = os.environ['SPOTIFY_REFRESH_TOKEN']
+        self._client_id = os.environ['SPOTIFY_CLIENT_ID']
+        self._client_secret = os.environ['SPOTIFY_CLIENT_SECRET']
+        self._token = None
+        self._requestor = Requestor()
+
+    def _get_new_token(self):
+        message = f'{self._client_id}:{self._client_secret}'
+        message_bytes = message.encode('ascii')
+        base64_bytes = base64.b64encode(message_bytes)
+        base64_message = 'Basic '+ base64_bytes.decode('ascii')
+        
+        headers = {'Authorization': base64_message}
+        
+        token = self._refresh_token
+        data = {'grant_type':'refresh_token', 'refresh_token': token}
     
-    headers = {'Authorization': base64_message}
-    token = os.environ['SPOTIFY_REFRESH_TOKEN']
-    data = {'grant_type':'refresh_token', 'refresh_token': token}
+        response = self._requestor.post_with_url_encoded(API_TOKEN_URL, data, headers)
+    
+        return response.json()['access_token']
+    
+    def get_current_song(self):
+        if not self._token:
+            token = self._get_new_token()
+            self._token = token
 
-    response = await requestor.post_with_url_encoded(API_TOKEN_URL, data, headers)
+        try:
+            token = self._token
+            current_url = 'https://api.spotify.com/v1/me/player/currently-playing'
+            headers = {"Authorization": f'Bearer {token}'}
+            r = self._requestor.simple_get(current_url, headers=headers)
+            
+            song_name = r.json()['item']['name']
+            song_authors = [author['name'] for author in r.json()['item']['artists']]
+            song = f'{", ".join(song_authors)} - {song_name}'
+            
+            return song
+        except:
+            return "Error"
+    
 
-    return response.json()['access_token']
+    #asyncio.run(get_new_token())
 
-
-asyncio.run(get_new_token())
+s = Spotify()
+song = s.get_current_song()
+print(song)
