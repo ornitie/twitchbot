6fce15041c5cb64b6b895d8ece3539c006be9392
Juan Pablo Rodriguez Morales <uncorreodeinternet@gmail.com>
2021-12-04 15:16:49 -0500

    Added what command

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
-import socketserver
+from dotenv import load_dotenv
+import os
+from twitchio.ext import commands
 
-class MyTCPHandler(socketserver.BaseRequestHandler):
-    """
-    The RequestHandler class for our server.
+load_dotenv()
 
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
-    """
 
-    def handle(self):
-        # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(1024).strip()
-        print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
-        # just send back the same data, but upper-cased
-        self.request.sendall(self.data.upper())
+class Bot(commands.Bot):
 
-if __name__ == "__main__":
-    HOST, PORT = "localhost", 443
+    def __init__(self):
+        super().__init__(token= os.environ['OAUTH'], prefix='!', initial_channels=['ornitie'])
 
-    # Create the server, binding to localhost on port 9999
-    server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
+    async def event_ready(self):
+        print(f'Logged in as | {self.nick}')
 
-    # Activate the server; this will keep running until you
-    # interrupt the program with Ctrl-C
-    print("running...")
-    server.serve_forever()
+    async def event_message(self, message):
+        if message.echo:
+            return
+
+        print(message.content)
+        await self.handle_commands(message)
+
+    @commands.command(name='hello')
+    async def hello(self, ctx: commands.Context):
+        print("hellooo")
+        await ctx.send(f'Hello {ctx.author.name}!')
+
+    @commands.command(name='what')
+    async def hello(self, ctx: commands.Context):
+        """ Create a command as a response to the question fo what we're doing rn """
+
+        print("Someone asked...")
+        await ctx.send(f'Hello {ctx.author.name}! Right now I\'m just snooping the Twitch API using twitchio')
+
+
+bot = Bot()
+bot.run()
